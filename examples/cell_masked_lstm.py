8932d18376aa1f9715f82efc7b63e09dfa466c84
andhus <andhus@kth.se>
2017-07-27 10:01:40 +0200

    cleaned up examples

--- examples/cell_masked_lstm.py
+++ examples/cell_masked_lstm.py
@@ -6,9 +6,11 @@ from keras.engine import Model
 
 from extkeras.layers.recurrent import CellMaskedLSTM
 
+n_timesteps = 5
+n_features = 3
 n_units = 2
 
-features_and_cell_mask = Input((5, 5))
+features_and_cell_mask = Input((n_timesteps, n_features + n_units))
 cell_masked_lstm = CellMaskedLSTM(
     units=n_units,
     output_cells=True,  # to be able to inspect that the cells are masked
@@ -17,15 +19,7 @@ cell_masked_lstm = CellMaskedLSTM(
 state_cell_sequence = cell_masked_lstm(features_and_cell_mask)
 model = Model(inputs=features_and_cell_mask, outputs=state_cell_sequence)
 
-features_data = np.array([
-    [
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-        [1.0, 2.0, 3.0],
-    ]
-])
+features_data = np.random.randn(1, n_timesteps, n_features)
 cell_mask_data = np.array([
     [
         [0., 0.],
@@ -35,6 +29,7 @@ cell_mask_data = np.array([
         [0., 1.],
     ]
 ])
+assert cell_mask_data.shape == (1, n_timesteps, n_units)
 feature_and_cell_mask_data = np.concatenate([features_data, cell_mask_data], 2)
 
 state_cell_sequence_data = model.predict(feature_and_cell_mask_data)
--- examples/phased_lstm_cell_mask.py
+++ examples/phased_lstm_cell_mask.py
@@ -1,6 +1,5 @@
 from __future__ import division, print_function
 
-
 import numpy as np
 from keras.engine import Input
 from keras.engine import Model
@@ -8,18 +7,16 @@ from keras.layers import TimeDistributed
 from extkeras.layers.recurrent import PhasedLSTMCellMask
 
 n_samples = 3
-sequence_length = 10
-units = 4
+n_timesteps = 10
+n_units = 4
 
-time = Input((sequence_length, 1))
+time = Input((n_timesteps, 1))
 
-cell_mask_layer = TimeDistributed(PhasedLSTMCellMask(units))
+cell_mask_layer = TimeDistributed(PhasedLSTMCellMask(n_units))
 cell_mask = cell_mask_layer(time)
+model = Model(inputs=time, outputs=cell_mask)
 
-model = Model(
-    inputs=time,
-    outputs=cell_mask
-)
-
-time_arr = np.arange(sequence_length).reshape((1, -1, 1)).repeat(n_samples, axis=0)
+time_arr = np.arange(n_timesteps).reshape((1, -1, 1)).repeat(n_samples, axis=0)
 mask = model.predict(time_arr)
+
+# TODO add plot of mask!
