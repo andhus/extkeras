53a3991f6c1167ddd3c4ac93dbe8053986c99d01
andhus <andhus@kth.se>
2017-07-28 18:43:40 +0200

    someting went a bit wrong

--- examples/distribution/mog.py
+++ examples/distribution/mog.py
@@ -5,7 +5,7 @@ from keras import Input
 from keras.engine import Model
 from keras.layers import Lambda, TimeDistributed
 
-from extkeras.layers.distribution import MoG1DParams, neg_log_mog1dpdf
+from extkeras.layers.distribution import MoGParams1D, neg_log_mog_pdf_1d
 
 n_timesteps = 3
 n_features = 5
@@ -15,10 +15,10 @@ n_components = 2
 features = Input((n_timesteps, n_features), name='features')
 target = Input((n_timesteps, 1), name='target')
 
-params_layer = TimeDistributed(MoG1DParams(components=n_components))
+params_layer = TimeDistributed(MoGParams1D(components=n_components))
 params = params_layer(features)
 
-loss_layer = Lambda(lambda y_true_pred: neg_log_mog1dpdf(*y_true_pred))
+loss_layer = Lambda(lambda y_true_pred: neg_log_mog_pdf_1d(*y_true_pred))
 loss = loss_layer([target, params])
 
 params_and_loss_model = Model(inputs=[target, features], outputs=[params, loss])
--- extkeras/layers/distribution.py
+++ extkeras/layers/distribution.py
@@ -1,5 +1,7 @@
 from __future__ import division, print_function
 
+import abc
+
 import numpy as np
 
 from keras import backend as K
@@ -29,49 +31,86 @@ class ScaledExponential(FunctionalBlock):
         return self.scale * K.exp(x) + self.epsilon
 
 
-class ParametersActivationBase(FunctionalBlock):
-    # TODO move split params to baseclass...
-    pass
+class MixtureParamsActivationBase(FunctionalBlock):
+    __metaclass__ = abc.ABCMeta
 
+    param_type_names = ('mixture_weight',)
+    n_param_types = len(param_type_names)
 
-class MoGParams1DActivation(FunctionalBlock):
-
-    sub_param_names = ('mu', 'sigma', 'phi')
-    n_sub_params = len(sub_param_names)
-
-    def __init__(
-        self,
-        components,
-        mu_activation=None,
-        sigma_activation=None,
-    ):
-        self.components = components
-        self.mu_activation = mu_activation or (lambda x: x)
-        self.sigma_activation = sigma_activation or ScaledExponential()
-        self.phi_activation = softmax
+    def __init__(self):
+        self.mixture_weight_activation = softmax
 
     @classmethod
     def split_params(cls, x):
+        """Splits input tensor into the """
         dim = x.shape[-1].value
-        if not dim % cls.n_sub_params == 0:
-            raise ValueError('')
-        components = dim // cls.n_sub_params
-        mu_ = x[..., :components]
-        sigma_ = x[..., components:components * 2]
-        phi_ = x[..., components*2:components * 3]
+        if not dim % cls.n_param_types == 0:
+            raise ValueError(
+                'this activation must be given tensor with last dimension'
+                'even dividable with number of parameter types: {}'.format(
+                    cls.n_param_types
+                )
+            )
+        components = dim // cls.n_param_types
+        param_types = [
+            x[i*components:(i+1)*components] for i in range(cls.n_param_types)
+        ]
 
-        return mu_, sigma_, phi_
+        return param_types
+
+    @abc.abstractmethod
+    def __call__(self, x):
+        """apply activation function on input tensor"""
+        pass
+
+    @classmethod
+    def loss(cls, y_true, y_pred):
+        """Implementation of standard loss for this this distribution normally
+        -log(pdf(y_true)) where pdf is parameterized by y_pred
+        """
+        raise NotImplementedError('')
+
+
+class MoGParams1DActivation(MixtureParamsActivationBase):
+
+    param_type_names = ('mixture_weight', 'mu', 'sigma')
+    n_param_types = len(param_type_names)
+
+    def __init__(
+        self,
+        mu_activation=None,
+        sigma_activation=None,
+    ):
+        super(MoGParams1DActivation, self).__init__()
+        self.mu_activation = mu_activation or (lambda x: x)
+        self.sigma_activation = sigma_activation or ScaledExponential()
 
     def __call__(self, x):
-        _mu, _sigma, _phi = self.split_params(x)
+        _mixture_weights, _mu, _sigma = self.split_params(x)
+        mixture_weights = self.mixture_weight_activation(_mixture_weights)
         mu = self.mu_activation(_mu)
         sigma = self.sigma_activation(_sigma)
-        phi = self.phi_activation(_phi)
 
-        return concatenate([mu, sigma, phi], axis=-1)
+        return concatenate([mixture_weights, mu, sigma], axis=-1)
+
+    @classmethod
+    def loss(cls, y_true, y_pred):
+        """TODO document and check dims of inputs
+        """
+        mixture_weights, mu, sigma, = cls.split_params(y_pred)
+        norm = 1. / (np.sqrt(2. * np.pi) * sigma)
+        exponent = -(
+            K.square(y_true - mu) / (2. * K.square(sigma)) -
+            K.log(mixture_weights) -
+            K.log(norm)
+        )
+        return -K.logsumexp(exponent, axis=-1)
 
 
-class MoG1DParams(Dense):
+neg_log_mog_pdf_1d = MoGParams1DActivation.loss
+
+
+class MoGParams1D(Dense):
 
     def __init__(
         self,
@@ -79,24 +118,11 @@ class MoG1DParams(Dense):
         mu_activation=None,
         sigma_activation=None
     ):
-        super(MoG1DParams, self).__init__(
+        super(MoGParams1D, self).__init__(
             units=3 * components,
             activation=MoGParams1DActivation(
-                components=components,
                 mu_activation=mu_activation,
                 sigma_activation=sigma_activation,
             )
         )
         self.n_components = components
-
-
-def neg_log_mog1dpdf(y_true, y_pred):
-    """TODO document and check dims of inputs"""
-    mu, sigma, phi = MoGParams1DActivation.split_params(y_pred)
-    norm = 1. / (np.sqrt(2. * np.pi) * sigma)
-    exponent = -(
-        K.square(y_true - mu) / (2. * K.square(sigma)) -
-        K.log(phi) -
-        K.log(norm)
-    )
-    return -K.logsumexp(exponent, axis=-1)
